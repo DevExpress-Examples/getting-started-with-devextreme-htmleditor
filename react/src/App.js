583159bac21610dad4613ad77c6b7f1558e9a359
czernous-devexpress <anton.chernous@devexpress.com>
2021-04-30 13:57:09 +0300

    fix images, move markup out of js files

--- react/src/App.js
+++ react/src/App.js
@@ -25,7 +25,6 @@ const headerValues = [false, 1, 2, 3, 4, 5];
 
 const App = () => {
     return (
-        <>
             <HtmlEditor height="725px" defaultValue={markup} valueType="html">
                 <MediaResizing enabled={true} />
                 <Toolbar multiline={true}>
@@ -69,8 +68,61 @@ const App = () => {
                     <Item name="insertColumnRight" />
                     <Item name="deleteColumn" />
                 </Toolbar>
+                
+                <div>
+                    <h2>
+                        <img src="HtmlEditor.svg" alt="HtmlEditor"/>
+                        Formatted Text Editor (HTML Editor)
+                    </h2>
+                    <br/>
+                    <p>DevExtreme JavaScript HTML Editor is a client-side WYSIWYG text editor that allows its users to format textual and visual content and store it as HTML or Markdown.</p>
+                    <p>Supported features:</p>
+                    <ul>
+                        <li>Inline formats:
+                            <ul>
+                                <li><strong>Bold</strong>, <em>italic</em>, <s>strikethrough</s> text formatting</li>
+                                <li>Font, size, color changes (HTML only)</li>
+                            </ul>
+                        </li>
+                        <li>Block formats:
+                            <ul>
+                                <li>Headers</li>
+                                <li>Text alignment</li>
+                                <li>Lists (ordered and unordered)</li>
+                                <li>Code blocks</li>
+                                <li>Quotes</li>
+                            </ul>
+                        </li>
+                        <li>Custom formats</li>
+                        <li>HTML and Markdown support</li>
+                        <li>Mail-merge placeholders (for example, %username%)</li>
+                        <li>Adaptive toolbar for working images, links, and color formats</li>
+                        <li>Insert images as a link or base64 (drag and drop images to convert them to base64)</li>
+                        <li>Copy-paste rich content (unsupported formats are removed)</li>
+                        <li>Tables support</li>
+                    </ul>
+                    <br/>
+                    <p>Supported frameworks and libraries</p>
+                    <table>
+                        <tr>
+                            <td><strong>jQuery</strong></td>
+                            <td style="text-align: right;">v2.1 - v2.2 and v3.x</td>
+                        </tr>
+                        <tr>
+                            <td><strong>Angular</strong></td>
+                            <td style="text-align: right;">v7.0.x - v10.0.x</td>
+                        </tr>
+                        <tr>
+                            <td><strong>React</strong></td>
+                            <td style="text-align: right;">v16.2+</td>
+                        </tr>
+                        <tr>
+                            <td><strong>Vue</strong></td>
+                            <td style="text-align: right;">v2.6.3+</td>
+                        </tr>
+                    </table>
+                </div>
             </HtmlEditor>
-        </>
     );
 };
 export default App;
