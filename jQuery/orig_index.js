f7ca8d8ce2c775f3fec2e28cc55764f53ac455c4
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2025-10-01 13:32:54 +0300

    Update to 25.1.3+

---
+++ jQuery/orig_index.js
@@ -0,0 +1,66 @@
+$(function () {
+    $("#html-editor").dxHtmlEditor({
+        valueType: "html",
+        toolbar: {
+            items: [
+                "undo",
+                "redo",
+                "separator",
+                {
+                    name: "size",
+                    acceptedValues: [ "8pt", "10pt", "12pt", "14pt", "18pt", "24pt", "36pt" ]
+                },
+                {
+                    name: "font",
+                    acceptedValues: [ "Arial", "Georgia", "Tahoma", "Times New Roman", "Verdana" ]
+                },
+                "separator",
+                "bold",
+                "italic",
+                "strike",
+                "underline",
+                "separator",
+                "alignLeft",
+                "alignCenter",
+                "alignRight",
+                "alignJustify",
+                "separator",
+                "orderedList",
+                "bulletList",
+                "separator",
+                {
+                    name: "header",
+                    acceptedValues: [false, 1, 2, 3, 4, 5]
+                },
+                "separator",
+                "color",
+                "background",
+                "separator",
+                "link",
+                "image",
+                "separator",
+                "clear",
+                "codeBlock",
+                "blockquote",
+                "separator",
+                "insertTable",
+                "deleteTable",
+                "insertRowAbove",
+                "insertRowBelow",
+                "deleteRow",
+                "insertColumnLeft",
+                "insertColumnRight",
+                "deleteColumn",
+                "cellProperties",
+                "tableProperties"
+            ],
+            multiline: true
+        },
+        mediaResizing: {
+            enabled: true
+        },
+        tableContextMenu: {
+            enabled: true
+        }
+    })
+});
